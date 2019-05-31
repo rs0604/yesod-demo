c95478634b1ab459e3c4e3a2324b7189374da2b9
rs0604 <shima19850604mail@gmail.com>
2019-05-31 17:43:08 +0900

    Yesod DB接続の設定

--- provision/yesod_init.sh
+++ provision/yesod_init.sh
@@ -9,5 +9,11 @@ cd rsyesodserver
 stack install yesod-bin --install-ghc
 stack build
 
+# DB設定を追加
+export YESOD_MYSQL_USER='rsyesodserver'
+export YESOD_MYSQL_PASSWORD='UgwD9_hwn3'
+export YESOD_MYSQL_HOST='localhost'
+export YESOD_MYSQL_DATABASE='yesod_db'
+
 # yesod 起動
 stack exec -- yesod devel
